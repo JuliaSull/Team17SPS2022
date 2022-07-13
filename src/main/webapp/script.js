1e548d846590ae1be9aec2e623d123af52013295
kunchen7 <105747316+kunchen7@users.noreply.github.com>
2022-07-13 11:07:12 -0400

    Update script.js

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -11,3 +11,20 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+function filter(){
+    var input=document.getElementById("filter_in");
+    var filter=input.value.toLowerCase();
+    var all_d=document.getElementById("all_data");
+    var li=all_d.getElementsByTagName('li');
+    var i;
+    for(i=0;i<li.length;i++){
+        var textv=li[i].getElementsByTagName("a")[0].textContent || 
+        li[i].getElementsByTagName("a")[0].innerText;
+        if(textv.toLowerCase().indexOf(filter)>=0){
+            li[i].style.display="";
+        }
+        else{
+            li[i].style.display="none";
+        }
+    }
+}
