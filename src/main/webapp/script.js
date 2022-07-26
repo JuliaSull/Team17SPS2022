a3e5b2b7e11ef89b42d5917b557bd92096a01e1b
5100-chap <A01275100@tec.mx>
2022-07-26 00:36:30 -0500

    Incorporates Post with the frontend

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -24,109 +24,121 @@
 // }
 
 let getPosts = function(filters) {
-    return {
-      "Post": [
-          {
-              "Title": "Test Title",
-              "Timestamp": "23409872349",
-              "Content": "lorem ipsum dolor sit amet consectetur adipiscing elit proin dictumst enim etiam nisi neque suspendisse vel lectus phasellus elementum magna hendrerit iaculis maximus tempor mollis purus a odio gravida euismod ad nibh torquent justo tellus duis nec eleifend pretium nascetur ornare nam ex erat bibendum dui aptent himenaeos leo tincidunt pellentesque tempus id commodo habitasse laoreet ligula conubia nisl ante donec molestie vestibulum nostra augue arcu primis feugiat ut interdum accumsan quisque vitae finibus penatibus consequat venenatis praesent inceptos nulla taciti varius rutrum ac congue in imperdiet et condimentum auctor vehicula montes nunc litora quam velit urna eget ultricies quis",
-              "Tags": [
-                  "Depression",
-                  "PTSD",
-                  "Trauma"
-              ]
-          },
-          {
-              "Title": "Test Title 2",
-              "Timestamp": "23409872349",
-              "Content": "lorem ipsum dolor sit amet consectetur adipiscing elit nisi condimentum venenatis tellus pulvinar sollicitudin nullam tortor risus est penatibus mi lectus nulla rutrum in hac finibus maecenas cursus iaculis eget primis rhoncus habitasse commodo cubilia aptent facilisi habitant egestas feugiat nascetur ligula magna viverra ornare suspendisse fusce praesent volutpat suscipit conubia laoreet magnis sed mollis nisl et lacinia natoque imperdiet dapibus litora potenti aliquet cras fermentum ad mauris massa taciti morbi nam gravida enim maximus ridiculus eros pharetra integer congue diam vestibulum inceptos vel parturient efficitur porttitor platea curae dictum scelerisque a mus posuere bibendum consequat torquent quam vulputate aenean",
-              "Tags": [
-                  "Tension",
-                  "Harrashment"
-              ]
-          },
-          {
-            "Title": "Test Title 3",
-            "Timestamp": "23409872340",
-            "Content": "lorem ipsum dolor sit amet consectetur adipiscing elit taciti porttitor primis diam tempor conubia pulvinar habitasse porta aliquet urna vulputate integer metus nascetur risus massa eu neque leo vivamus parturient volutpat id congue aliquam facilisi libero etiam arcu fermentum efficitur sagittis pellentesque pretium accumsan elementum aptent magna auctor fusce fringilla finibus faucibus senectus duis nullam enim curabitur turpis sapien praesent posuere inceptos semper fames facilisis nulla sollicitudin hendrerit mollis dictum potenti habitant scelerisque nostra platea vehicula ullamcorper imperdiet eget litora lacus sociosqu morbi hac ridiculus tincidunt nisl orci ultrices per interdum tellus justo a ad velit vel tortor lacinia torquent",
+  return {
+    "Post": [
+        {
+            "Title": "Test Title",
+            "Timestamp": "23409872349",
+            "Content": "lorem ipsum dolor sit amet consectetur adipiscing elit proin dictumst enim etiam nisi neque suspendisse vel lectus phasellus elementum magna hendrerit iaculis maximus tempor mollis purus a odio gravida euismod ad nibh torquent justo tellus duis nec eleifend pretium nascetur ornare nam ex erat bibendum dui aptent himenaeos leo tincidunt pellentesque tempus id commodo habitasse laoreet ligula conubia nisl ante donec molestie vestibulum nostra augue arcu primis feugiat ut interdum accumsan quisque vitae finibus penatibus consequat venenatis praesent inceptos nulla taciti varius rutrum ac congue in imperdiet et condimentum auctor vehicula montes nunc litora quam velit urna eget ultricies quis",
+            "Tags": [
+                "Depression",
+                "PTSD",
+                "Trauma"
+            ]
+        },
+        {
+            "Title": "Test Title 2",
+            "Timestamp": "23409872349",
+            "Content": "lorem ipsum dolor sit amet consectetur adipiscing elit nisi condimentum venenatis tellus pulvinar sollicitudin nullam tortor risus est penatibus mi lectus nulla rutrum in hac finibus maecenas cursus iaculis eget primis rhoncus habitasse commodo cubilia aptent facilisi habitant egestas feugiat nascetur ligula magna viverra ornare suspendisse fusce praesent volutpat suscipit conubia laoreet magnis sed mollis nisl et lacinia natoque imperdiet dapibus litora potenti aliquet cras fermentum ad mauris massa taciti morbi nam gravida enim maximus ridiculus eros pharetra integer congue diam vestibulum inceptos vel parturient efficitur porttitor platea curae dictum scelerisque a mus posuere bibendum consequat torquent quam vulputate aenean",
             "Tags": [
                 "Tension",
-                "Harrashment",
-                "LGBTQ+"
+                "Harrashment"
             ]
-        }
-      ]
-    }
+        },
+        {
+          "Title": "Test Title 3",
+          "Timestamp": "23409872340",
+          "Content": "lorem ipsum dolor sit amet consectetur adipiscing elit taciti porttitor primis diam tempor conubia pulvinar habitasse porta aliquet urna vulputate integer metus nascetur risus massa eu neque leo vivamus parturient volutpat id congue aliquam facilisi libero etiam arcu fermentum efficitur sagittis pellentesque pretium accumsan elementum aptent magna auctor fusce fringilla finibus faucibus senectus duis nullam enim curabitur turpis sapien praesent posuere inceptos semper fames facilisis nulla sollicitudin hendrerit mollis dictum potenti habitant scelerisque nostra platea vehicula ullamcorper imperdiet eget litora lacus sociosqu morbi hac ridiculus tincidunt nisl orci ultrices per interdum tellus justo a ad velit vel tortor lacinia torquent",
+          "Tags": [
+              "Tension",
+              "Harrashment",
+              "LGBTQ+"
+          ]
+      }
+    ]
   }
+}
 
 Posts = getPosts();
 
 function storePost() {
-    // store the post in the database
-    //     getElementById("post-conent").value
-    //     getElementById("isADHD").checked
-    //     Stick them in a javascript object, jsonify it, and store it
-    // .then()
-    // Posts = getPosts();
-    // refreshTimeline();
+  // store the post in the database
+  //     getElementById("post-conent").value
+  //     getElementById("isADHD").checked
+  //     Stick them in a javascript object, jsonify it, and store it
+  // .then()
+  // Posts = getPosts();
+  // refreshTimeline();
 }
 
 function refreshTimeline() {
-    for(let i = 0; i < Posts.Post.length; i++){
-        const ul = document.getElementById('wrapper');
-        const li = document.createElement('li');
-        li.classList.add('notes')
-        console.log(li);
-        ul.append(li)
-    
-        const p = document.createElement('p');
-        p.innerText = Posts.Post[i].Title;
-        p.classList.add('title')
-    
-        li.append(p);
-    
-        const span = document.createElement('span');
-        span.innerText= Posts.Post[i].Content;
-        span.classList.add('description');
-        li.append(span)
-    
-        const tags = document.createElement('ul');
-        tags.classList.add('tags')
-    
-        console.log(Posts.Post[0].Tags[1])
-    
-        for(let j = 0; j < Posts.Post[i].Tags.length; j++){
-            console.log(Posts.Post[i].Tags[j])
-            const subTags = document.createElement('li');
-            const buttons = document.createElement('button')
-            subTags.append(buttons);
-            buttons.innerText = Posts.Post[i].Tags[j];
-            tags.append(subTags);
-        }
-    
-        li.append(tags);
-     }
- }
-
-
- function filter() {
-  var input, output, data, tr, td, filtered;
-  input = document.getElementById("search");
-  output = input.value.toLowerCase();
-  data = document.getElementById("posts");
-  tr = data.getElementsByTagName("tr");
-  for (var i = 1; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td");
-    for (var j = 0; j < td.length; j++) {
-      filtered = tr[i].getElementsByTagName("td")[j];
-      if (filtered) {
-        if (filtered.innerHTML.toLowerCase().indexOf(output) >= 0) {
-          tr[i].style.display = "";
-        }
-        else{
-            tr[i].style.display="none";
-        } 
+  for(let i = 0; i < Posts.Post.length; i++){
+      const ul = document.getElementById('wrapper');
+      const li = document.createElement('li');
+      li.classList.add('notes')
+      console.log(li);
+      ul.append(li)
+  
+      const p = document.createElement('p');
+      p.innerText = Posts.Post[i].Title;
+      p.classList.add('title')
+  
+      li.append(p);
+  
+      const span = document.createElement('span');
+      span.innerText= Posts.Post[i].Content;
+      span.classList.add('description');
+      li.append(span)
+  
+      const tags = document.createElement('ul');
+      tags.classList.add('tags')
+  
+      console.log(Posts.Post[0].Tags[1])
+  
+      for(let j = 0; j < Posts.Post[i].Tags.length; j++){
+          console.log(Posts.Post[i].Tags[j])
+          const subTags = document.createElement('li');
+          const buttons = document.createElement('button')
+          subTags.append(buttons);
+          buttons.innerText = Posts.Post[i].Tags[j];
+          tags.append(subTags);
       }
+  
+      li.append(tags);
+   }
+}
+
+function TagsSelected(){
+  var TagTypes = document.getElementsByName("Tags");
+  var TagSelected = [];
+  for (var i = 0; i < TagTypes.length; i++) {
+      if (TagTypes[i].checked) {
+          TagSelected.push(TagTypes[i].value);
+      }
+  }
+  var hiddenSelectedTags = document.getElementById("selectedTags");
+  hiddenSelectedTags.value = TagSelected.join(",");
+  form.submit();
+}
+
+function filter() {
+var input, output, data, tr, td, filtered;
+input = document.getElementById("search");
+output = input.value.toLowerCase();
+data = document.getElementById("posts");
+tr = data.getElementsByTagName("tr");
+for (var i = 1; i < tr.length; i++) {
+  td = tr[i].getElementsByTagName("td");
+  for (var j = 0; j < td.length; j++) {
+    filtered = tr[i].getElementsByTagName("td")[j];
+    if (filtered) {
+      if (filtered.innerHTML.toLowerCase().indexOf(output) >= 0) {
+        tr[i].style.display = "";
+      }
+      else{
+          tr[i].style.display="none";
+      } 
     }
   }
+}
 }
